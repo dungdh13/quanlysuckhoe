63365bce2366363e9f778d7de9e9debd9a6e05ed
Dang Dung <dungdh286@gmail.com>
2020-03-25 17:40:38 +0700

    Enter

--- users/forms.py
+++ users/forms.py
@@ -1,14 +1,24 @@
 from django import forms
 from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from .models import Profile, Todo
 from django.forms import ModelForm
 
 
+class UserLoginForm(AuthenticationForm):
+    
+    username= forms.CharField(label="Tên Đăng Nhập")
+    password= forms.CharField(max_length=32, widget=forms.PasswordInput,label="Mật khẩu")
+    class Meta:
+        model = User
+        fields =['username', 'password']
+
 
 class UserRegisterForm(UserCreationForm):
-    email = forms.EmailField(label="hộp thư của ban:")
-    username = forms.CharField(label="tên đangq nhập")
+    email = forms.EmailField(label="Địa chỉ email")
+    username= forms.CharField(label="Tên Đăng Nhập")
+    password1= forms.CharField(max_length=32, widget=forms.PasswordInput,label="Mật khẩu")
+    password2= forms.CharField(max_length=32, widget=forms.PasswordInput,label="Xác nhận mật khẩu")
     class Meta:
         model = User
         fields =['username', 'email', 'password1', 'password2']
@@ -16,7 +26,7 @@ class UserRegisterForm(UserCreationForm):
 
 
 class UserUpdateForm(forms.ModelForm):
-    email = forms.EmailField()
+    email = forms.EmailField(required=False)
 
     class Meta:
         model = User
@@ -24,6 +34,13 @@ class UserUpdateForm(forms.ModelForm):
 
 
 class ProfileUpdateForm(forms.ModelForm):
+    Ho_ten = forms.CharField(label="Họ và tên:",max_length=30, required=False)
+    sdt = forms.IntegerField(label="Số điện thoại", max_value=1000000000000000, required=False)
+    khoa_phong = forms.CharField(label="Khoa phòng:",max_length=30, required=False)
+    dia_chi = forms.CharField(label="Địa chỉ:",max_length=100,required=False)
+    MSBN = forms.IntegerField(label="Mã số bệnh nhân(bệnh nhân khai)", max_value = 1000000000000000, required=False)
+    cd_benh = forms.CharField(label="Chẩn đoán bệnh(bệnh nhân khai)",max_length=150, required=False)
+    lich_hen_kham = forms.DateField(label="Lịch hẹn khám(bệnh nhân khai)",required=False)
     class Meta:
         model = Profile
         fields =['Ho_ten','sdt','khoa_phong','dia_chi','MSBN','cd_benh','lich_hen_kham']
@@ -34,4 +51,4 @@ class ProfileUpdateForm(forms.ModelForm):
 class TodoForm(ModelForm):
     class Meta:
         model = Todo
-        fields = ['sot', 'ho', 'kho_tho','tiepxuc_f0','tiepxuc_f1','vung_dich','tt_banthan']
+        fields = ['sot', 'ho', 'kho_tho','dau_hong', 'met_moi','dau_dau','ret_run','dau_co','non','buon_non', 'tiepxuc_f0','tiepxuc_f1','vung_dich','tt_banthan']
--- users/views.py
+++ users/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect,get_object_or_404
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
+from django.contrib.auth import login, logout, authenticate
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm, UserLoginForm
 from .forms import TodoForm
 from .models import Todo
 from django.utils import timezone
@@ -9,6 +10,18 @@ from django.contrib.auth.models import User
 from django.db import IntegrityError
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
+
+def login_user(request):
+    if request.method == 'GET':
+        return render(request, 'users/login.html', {'form':UserLoginForm()})
+    else:
+        user = authenticate(request, username=request.POST['username'], password=request.POST['password'])
+        if user is None:
+            messages.success(request, f'Tên đăng nhập hoặc mật khẩu không không đúng!')
+            return render(request, 'users/login.html', {'form':UserLoginForm()})
+        else:
+            login(request, user)
+            return redirect('currenttodos')
 
 def register(request):
     if request.method == 'POST':
